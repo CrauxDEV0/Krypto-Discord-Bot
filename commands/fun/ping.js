4434aefe524b0930a7e142eed03edeec4108660b
CrauxDEV0 <96301244+CrauxDEV0@users.noreply.github.com>
2022-03-06 00:54:04 +0530

    Create ping.js

---
+++ commands/fun/ping.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name:'ping',
+    description:'get the user the ping of the bot',
+    run:async(client, message, args) => {
+        const latency = `\`\`\`ini\n[ ${client.ws.ping}ms ]\`\`\``;
+
+        let embed = new MessageEmbed()
+        embed.setTitle('Pong!')
+        embed.setDescription('')
+        embed.addField('Latency', latency, true)
+        embed.setThumbnail("https://media.discordapp.net/attachments/747094092596510841/767079159977082910/2102a19ea556e1d1c54f40a3eda0d775.gif")
+        embed.setColor('BLACK')
+        embed.setFooter('Made by Craux!')
+        embed.setTimestamp()
+        
+        message.channel.send({embeds:[embed]});
+    }
+}
