e366c1bdb3028a799a41204d24aef06668c73e48
CrauxDEV0 <96301244+CrauxDEV0@users.noreply.github.com>
2022-03-14 21:18:26 +0530

    Create help.js

---
+++ commands/info/help.js
@@ -0,0 +1,23 @@
+const discord = require('discord.js');
+const prefix = process.env.PREFIX;
+module.exports = {
+  name:'help',
+  usage:`${prefix}help`,
+  run:async(client, message, args) => {
+
+   let embed2 = new discord.MessageEmbed()
+   embed2.setTitle(`<:commands:917224510057480212> Help Commands`)
+   embed2.setDescription('')
+   embed2.addField(`<:smile:948281673273995345> Fun commands`,'`avatar`,`8ball`,`hack`',true)
+   embed2.addField(`<:image:948279032934461500> Image Commands`,'`clyde`,`alert`', true)
+   embed2.addField(`<a:Gamer:949746159956656128> Game Commands`,'`rps`',true)
+   embed2.addField(`<a:Money:927552113389756506>  Economy Commands`,'`bal`,`daily`,`beg`,`lb`',true)
+   embed2.addField(`<a:info:918880832716955658> Info Commands`,'`ping`,`help`,`botstats`',true)
+   embed2.setFooter('@ Help')
+   embed2.setColor('BLACK')
+   embed2.setTimestamp();
+
+   message.channel.send({embeds:[embed2]});
+
+  }
+}
